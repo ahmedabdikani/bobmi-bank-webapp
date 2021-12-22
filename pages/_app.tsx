c1b72139b91ad297382bf25b350a0c35646998e4
DecovicHunter <ahmed_abdikani@hotmail.com>
2021-12-22 20:12:50 +0800

    importing drawer dynamically

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { createBreakpoints } from '@chakra-ui/theme-tools';
+import dynamic from 'next/dynamic';
 
 import '@fontsource/montserrat/100.css';
 import '@fontsource/montserrat/200.css';
@@ -12,11 +13,14 @@ import '@fontsource/montserrat/600.css';
 import '@fontsource/montserrat/700.css';
 import '@fontsource/montserrat/800.css';
 import '@fontsource/montserrat/900.css';
+
 import '../styles/globals.css';
 
 import Layout from '../components/Layout';
-import Drawer from '../components/Drawer';
 import { DrawerProvider } from '../context/DrawerContext';
+const Drawer = dynamic(() => import('../components/Drawer'), {
+  ssr: false,
+});
 
 const colors = {
   brand: {
