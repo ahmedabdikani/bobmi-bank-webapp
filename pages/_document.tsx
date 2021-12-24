dfeef279614396d32d125cbdf943b22e0e9d361e
DecovicHunter <ahmed_abdikani@hotmail.com>
2021-12-25 03:08:31 +0800

    using google fonts rather than @fontsource

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,18 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='anonymous'
+          />
+          <link
+            href={
+              'https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800&display=swap'
+            }
+            rel='stylesheet'
+          />
           <link
             rel='apple-touch-icon'
             sizes='180x180'
