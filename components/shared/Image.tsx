06a08c83ce3d3972e95db5bab5e1683c4f40b1a9
DecovicHunter <ahmed_abdikani@hotmail.com>
2021-12-25 03:32:42 +0800

    update image component

--- components/shared/Image.tsx
+++ components/shared/Image.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Img as ChakraImage, ImageProps } from '@chakra-ui/react';
 
-const Image: React.FC<ImageProps> = (props) => {
-  const {
-    src,
-    width,
-    height,
-    objectFit = 'contain',
-    loading = 'lazy',
-    ...rest
-  } = props;
+const Image: React.FC<ImageProps> = ({
+  src,
+  width,
+  height,
+  objectFit = 'contain',
+  loading = 'lazy',
+  ...rest
+}) => {
   const newSrc = process?.env?.NEXT_PUBLIC_BASE_PATH
     ? `${process?.env?.NEXT_PUBLIC_BASE_PATH}${src}`
     : src;
   return (
     <ChakraImage
       src={newSrc}
-      // boxSize={width + 'px'}
       w={width + 'px'}
       maxH={height + 'px'}
       objectFit={objectFit}
